7ae78f729b0ca274e05dc4734f7f8690496ac345
star__hoshi <kensuke1751@gmail.com>
2016-05-08 08:55:21 +0900

    twitter 認証 -> 情報保存 -> musiced の url_scheme を起動

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160507183736) do
+ActiveRecord::Schema.define(version: 20160507233645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20160507183736) do
     t.string   "uid"
     t.string   "user_name"
     t.string   "avatar_url"
+    t.string   "token"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
